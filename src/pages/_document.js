e8cc9067959971c9c146683c1d1c16fbb70235d4
harish-gokul01 <harish.gokul@byjus.com>
2023-09-05 10:56:11 +0530

    feat: :zap: google analytics testing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,8 +16,24 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Varela&display=swap"
           rel="stylesheet"
         ></link>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PSLW2PBR');`,
+          }}
+        ></Script>
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSLW2PBR"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <Main />
         <NextScript />
       </body>
